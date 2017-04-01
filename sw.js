c459c35d26b7a562e357ff744e414b00306632d7
neal1991 <neal1991@sina.com>
2017-04-01 18:58:23 +0800

    modify the chache file path :bug:

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 var cacheName = 'subway';
 var filesToCache = [
 	'/',
-	'../index.html',
-	'../image/transfer.png'
+	'index.html',
+	'image/transfer.png'
 ];
 
 self.addEventListener('install', function(e) {
