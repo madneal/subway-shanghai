bea8bce147d4ff8d661386e966592dd4cafad6cb
dongne <neal.dong@oocl.com>
2017-04-06 14:44:59 +0800

    modify the gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -5,5 +5,10 @@ gulp.task('script', function() {
 	gulp.src(['app.js', 'alloy_finger.js'])
 	.pipe(uglify())
 	.pipe(gulp.dest('dist/'))
-})
+});
 
+gulp.task('auto', function() {
+	gulp.watch('app.js', ['script']);
+});
+
+gulp.task('default', ['script', 'auto'])
