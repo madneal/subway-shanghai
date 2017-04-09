88dd420f7dd4ff0428511290c467c016a5732052
neal1991 <neal1991@sina.com>
2017-04-09 13:50:58 +0800

    add scss :lipstick:

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var minifycss = require('gulp-minify-css');
 
 gulp.task('script', function() {
 	gulp.src(['app.js', 'alloy_finger.js'])
@@ -7,8 +9,19 @@ gulp.task('script', function() {
 	.pipe(gulp.dest('dist/'))
 });
 
+gulp.task('sass', function() {
+	return gulp.src('app.scss')
+		.pipe(sass())
+		.on('error', function(err) {
+			console.error('Error!', err.message);
+		})
+		.pipe(gulp.dest('dist/'))
+		.pipe(minifycss())
+		.pipe(gulp.dest('dist/'))
+})
+
 gulp.task('auto', function() {
 	gulp.watch('app.js', ['script']);
 });
 
-gulp.task('default', ['script', 'auto'])
+gulp.task('default', ['script', ,'sass', 'auto'])
