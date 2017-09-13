3464c9c94e59c8e2cce1ca84260b8bce8ac3a307
neal1991 <neal1991@sina.com>
2017-09-13 21:52:26 +0800

    rewrite wc.js

--- wc.js
+++ wc.js
@@ -1,19 +1,23 @@
-var fs = require('fs');
+const fs = require('fs');
 
-fs.readFile('wc.txt', 'utf-8', function(err, data) {
-	var arr = data.split('\n');
-	var stationInfo = {};
-	var reg = /alt="[\u4e00-\u9fa5]+/;
-	for (var i = 0; i < arr.length; i++) {
-		var lineData = arr[i];
+const trim = str => {
+	return str.replace(/^\s+|\s+$/gm, '');
+}
+
+fs.readFile('wc.txt', 'utf-8', (err, data) => {
+	const arr = data.split('\n');
+	const stationInfo = {};
+	const reg = /alt="[\u4e00-\u9fa5]+/;
+	for (let i = 0; i < arr.length; i++) {
+		const lineData = arr[i];
 		stationName = lineData.match(reg)[0];
 		stationName = stationName.replace(/alt="/, '');
-		var info = lineData.replace(reg, '');
+		const info = lineData.replace(reg, '');
 		info = info.replace(/\r/, '');
 		info = trim(info);
 		stationInfo[stationName] = info;
 	}
-	var jsonStr = JSON.stringify(stationInfo);
+	const jsonStr = JSON.stringify(stationInfo);
 	fs.writeFileSync('wc.json', jsonStr, 'utf8', function(err) {
 		if (err) {
 			console.log(err);
@@ -23,7 +27,3 @@ fs.readFile('wc.txt', 'utf-8', function(err, data) {
 	})
 	console.log('finished');
 })
-
-function trim(str) {
-	return str.replace(/^\s+|\s+$/gm, '');
-}
