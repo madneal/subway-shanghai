5612d644ba2154be39b0ad97838d34295fdba7c8
neal1991 <neal1991@sina.com>
2018-03-10 08:24:05 +0800

    add line path

--- src/constants.js
+++ src/constants.js
@@ -1,16 +1,18 @@
-const LINE1 = '1号线';
-const LINE2 = '2号线';
-const LINE2_EXTEND = '2号线东延线';
-const LINE3 = '3号线';
-const LINE4 = '4号线';
-const LINE5 = '5号线';
-const LINE6 = '6号线';
-const LINE7 = '7号线';
-const LINE8 = '8号线';
-const LINE9 = '9号线';
-const LINE10 = '10号线';
-const LINE11 = '11号线';
-const LINE12 = '12号线';
-const LINE13 = '13号线';
-const LINE16 = '16号线';
-const LINE17 = '17号线';
+export default lineNames({
+   LINE1: '1号线',
+   LINE2: '2号线',
+   LINE2_EXTEND: '2号线东延线',
+   LINE3: '3号线',
+   LINE4: '4号线',
+   LINE5: '5号线',
+   LINE6: '6号线',
+   LINE7: '7号线',
+   LINE8: '8号线',
+   LINE9: '9号线',
+   LINE10: '10号线',
+   LINE11: '11号线',
+   LINE12: '12号线',
+   LINE13: '13号线',
+   LINE16: '16号线',
+   LINE17: '17号线'
+});
