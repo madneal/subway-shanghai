3f0a6e246c2dc153e99d4921c8bffa5856d63e7e
neal1991 <neal1991@sina.com>
2018-03-10 15:10:44 +0800

    add tansfer in Station

--- src/views/Station.js
+++ src/views/Station.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import stations from '../data/stations.json'
+import transfers from '../data/transfers.json'
 
 class Station extends React.Component {
   render() {
     const stationEles = [];
+    const transferEles = [];
+
     for (let i = 0; i < stations.length; i++) {
       const station = stations[i];
       stationEles.push(
         <circle cx={station.cx} cy={station.cy} r="5" fill="white" stroke={station.stroke} id={station.id} key={station.id + i}></circle>
+      )
+    }
+
+    for (let i = 0; i < transfers.length; i++) {
+      const transfer = transfers[i];
+      transferEles.push(
+        <image x={transfer.x} y={transfer.y} dataId={station['data-id']} href="../../public/image/transfer.png" key={station['data-id'] + i}></image>
       )
     }
     return (
