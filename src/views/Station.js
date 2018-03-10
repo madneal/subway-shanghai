fc1d927c3a0af6959ee3caa0adbc8ad6cbe4cf36
neal1991 <neal1991@sina.com>
2018-03-10 15:59:13 +0800

    add onClick event and InfoCardDiv

--- src/views/Station.js
+++ src/views/Station.js
@@ -1,12 +1,32 @@
 import React from 'react'
 import stations from '../data/stations.json'
 import transfers from '../data/transfers.json'
-import transferPath from '../../public/image/transfer.png'
+import transferPath from '../imgs/transfer.png'
+import InfoCard from './InfoCard'
 
 class Station extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false
+    }
+    this.convertShow = this.convertShow.bind(this);
+  }
+
+  convertShow() {
+    this.setState(preState => {
+      show: !preState.show
+    });
+  }
+
   render() {
     const stationEles = [];
     const transferEles = [];
+    let InfoCardDiv = null;
+
+    if (this.state.show) {
+      InfoCardDiv = <InfoCard />
+    }
 
     for (let i = 0; i < stations.length; i++) {
       const station = stations[i];
@@ -21,10 +41,12 @@ class Station extends React.Component {
         <image x={transfer.x} y={transfer.y} dataid={transfer['data-id']} href={transferPath} key={transfer['data-id'] + i}></image>
       )
     }
+
     return (
-      <g>
+      <g onClick={this.convertShow}>
         {stationEles}
         {transferEles}
+        {InfoCardDiv}
       </g>
     )
   }
