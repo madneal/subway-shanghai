a991ba9c4ca4891ced1608a2bd268bacc609a5fb
neal1991 <neal1991@sina.com>
2018-03-10 19:51:38 +0800

    add timesheet

--- src/views/InfoCard.js
+++ src/views/InfoCard.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
 class InfoCard extends React.Component {
+  constructor() {
+    super(props);
+
+  }
   render() {
     return (
     <div className="info-card">
-
+        <div className="header"></div>
+        <div className="container"></div>
     </div>
     )
   }
---
+++ src/views/TimeSheet.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class TimeSheet extends React.Component {
+  constructor() {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="timesheet"></div>
+    )
+  }
+}
