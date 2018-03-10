9d55e5d49af0683c053b11a75e688e8951f72c86
neal1991 <neal1991@sina.com>
2018-03-10 13:55:06 +0800

    modify title

--- src/views/line.js
+++ src/views/line.js
@@ -6,19 +6,19 @@ class Line extends React.Component {
   render() {
     console.dir(linePath);
     const linePaths = [];
+
     for (const key in linePath) {
       console.log(key);
       const path = linePath[key];
       const lineNum = key.match(/\d+/)[0];
       const color = lineColor[lineNum];
-      linePaths.push(
-        <path d={path} fill="none" strokeWidth="6" stroke={color} key={key}></path>);
+      linePaths.push(<path d={path} fill="none" strokeWidth="6" stroke={color} key={key}></path>);
     }
+
     return (
       <g>{linePaths}</g>
-      
     )
   }
 }
 
-export default Line
+export default Line;
