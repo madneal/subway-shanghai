ce9b68b9ea5d6b621ed12778abb53c42f0eda359
neal1991 <neal1991@sina.com>
2018-03-11 17:25:36 +0800

    add entance and elevator icons :lipstick:

--- src/views/InfoCard.js
+++ src/views/InfoCard.js
@@ -2,12 +2,18 @@ import React from 'react'
 import '../styles/Station.css'
 import wcActive from '../imgs/wc.png'
 import wcInactive from '../imgs/wc0.png'
+import elevatorActive from '../imgs/elevator.png'
+import elevatorInactive from '../imgs/elevator0.png'
+import entranceActive from '../imgs/exit.png'
+import entranceInactive from '../imgs/exit0.png'
 
 class InfoCard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      wc: false
+      wc: false,
+      elevator: false,
+      entrance: false
     }
   }
 
@@ -19,6 +25,12 @@ class InfoCard extends React.Component {
     }
   }
 
+  changeState(stateName) {
+    this.setState({
+      stateName: !this.state[stateName]
+    })
+  }
+
   render() {
     const infoCard = this.props.infoCard;
     return (
@@ -26,7 +38,9 @@ class InfoCard extends React.Component {
         <div className="header">
           {infoCard.stationName}
           <span className="icons">
-            <img src={this.state.wc ? wcActive : wcInactive}/>
+            <img src={this.state.wc ? wcActive : wcInactive} alt="卫生间"/>
+            <img src={this.state.elevator ? elevatorActive : elevatorInactive} alt="无障碍电梯"/>
+            <img src={this.state.entrance ? entranceActive : entranceInactive} alt="出入口"/>          
           </span>
         </div>
         <div className="container"></div>
