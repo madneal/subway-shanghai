6d47746ce77c25bd4245c332f74ef6a1a9819a3d
neal1991 <neal1991@sina.com>
2018-03-13 22:46:50 +0800

    timesheet format

--- src/views/Station.js
+++ src/views/Station.js
@@ -37,65 +37,76 @@ class Station extends React.Component {
       // const firstTime = ele['first_time'];
       // const lastTime = ele['last_time'];
       // const weekday = ele['last_time_desc'].weekday;
-      formatedTimesheet[line] = {
+      const data = {
         firstTime: ele.first_time,
-        last_time: ele.last_time,
+        lastTime: ele.last_time,
         weekday: ele.last_time_desc ? JSON.parse(ele.last_time_desc).weekday : null,
         description: ele.description
+      };
+      if (!formatedTimesheet[line]) {
+        formatedTimesheet[line] = [data];
+      } else {
+        formatedTimesheet[line] = formatedTimesheet[line].concat(data);
       }
     }
+    // formatedTimesheet[line] = {
+    //   firstTime: ele.first_time,
+    //   last_time: ele.last_time,
+    //   weekday: ele.last_time_desc ? JSON.parse(ele.last_time_desc).weekday : null,
+    //   description: ele.description
+    // }
     return formatedTimesheet;
   }
 
-  convertShow(e) {
-    const attributes = e.target.attributes;
-    const stationName = this.getAttVal(attributes, 'id') || this.getAttVal(attributes, 'dataid');
-    const x = e.target.x ? e.target.x.baseVal.valueAsString : e.target.cx.baseVal.valueAsString;
-    const y = e.target.y ? e.target.y.baseVal.valueAsString : e.target.cy.baseVal.valueAsString;    
-    const position = {
-      x: x,
-      y: y
-    };
-    const statId = this.getAttVal(attributes, 'statid');
-    let timesheet = null;  
-    timesheet = stationInfos[statId].timesheet;
-    timesheet = this.formatTimesheet(timesheet);
-    const infoCard = {
-      show: !this.state.show,
-      stationName: stationName,
-      stationPosition: position,
-      statId: statId,
-      timesheet: timesheet
-    }
-    this.props.convertShowInfoCard(infoCard);
+convertShow(e) {
+  const attributes = e.target.attributes;
+  const stationName = this.getAttVal(attributes, 'id') || this.getAttVal(attributes, 'dataid');
+  const x = e.target.x ? e.target.x.baseVal.valueAsString : e.target.cx.baseVal.valueAsString;
+  const y = e.target.y ? e.target.y.baseVal.valueAsString : e.target.cy.baseVal.valueAsString;
+  const position = {
+    x: x,
+    y: y
+  };
+  const statId = this.getAttVal(attributes, 'statid');
+  let timesheet = null;
+  timesheet = stationInfos[statId].timesheet;
+  timesheet = this.formatTimesheet(timesheet);
+  const infoCard = {
+    show: !this.state.show,
+    stationName: stationName,
+    stationPosition: position,
+    statId: statId,
+    timesheet: timesheet
   }
+  this.props.convertShowInfoCard(infoCard);
+}
 
-  render() {
-    const stationEles = [];
-    const transferEles = [];
-    let InfoCardDiv = null;
+render() {
+  const stationEles = [];
+  const transferEles = [];
+  let InfoCardDiv = null;
 
-    for (let i = 0; i < stations.length; i++) {
-      const station = stations[i];
-      stationEles.push(
-        <circle cx={station.cx} cy={station.cy} r="5" fill="white" stroke={station.stroke} id={station.id} statid={station.statid} key={station.id + i}></circle>
-      )
-    }
-
-    for (let i = 0; i < transfers.length; i++) {
-      const transfer = transfers[i];
-      transferEles.push(
-        <image x={transfer.x} y={transfer.y} dataid={transfer['data-id']} href={transferPath} statid={transfer.statid} key={transfer['data-id'] + i}></image>
-      )
-    }
-
-    return (
-      <g onClick={e => this.convertShow(e)}>
-        {stationEles}
-        {transferEles}
-      </g>
+  for (let i = 0; i < stations.length; i++) {
+    const station = stations[i];
+    stationEles.push(
+      <circle cx={station.cx} cy={station.cy} r="5" fill="white" stroke={station.stroke} id={station.id} statid={station.statid} key={station.id + i}></circle>
     )
   }
+
+  for (let i = 0; i < transfers.length; i++) {
+    const transfer = transfers[i];
+    transferEles.push(
+      <image x={transfer.x} y={transfer.y} dataid={transfer['data-id']} href={transferPath} statid={transfer.statid} key={transfer['data-id'] + i}></image>
+    )
+  }
+
+  return (
+    <g onClick={e => this.convertShow(e)}>
+      {stationEles}
+      {transferEles}
+    </g>
+  )
+}
 }
 
 export default Station
