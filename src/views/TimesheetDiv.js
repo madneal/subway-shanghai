4da2fd1266d51842fbbab1828922e0718c944ed3
Neal Caffery <neal1991@sina.com>
2018-03-13 09:25:59 +0800

    add num format :art:

--- src/views/TimesheetDiv.js
+++ src/views/TimesheetDiv.js
@@ -1,7 +1,45 @@
 import React from 'react'
 
 class TimesheetDiv extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  
+  formatNum(num) {
+    if (num < 10) {
+      num = '0' + num;
+    }
+    return '' + num;
+  }
+  
+  addTime(currentTime, minutes) {
+    const timeArr = currentTime.split(':');
+    let hour = +timeArr[0];
+    let minute = +timeArr[1];
+    minute = minute + minutes;
+    if (minute > 60) {
+      hour = hour + minute / 60;
+      minute = minute % 60;
+    } 
+    if (hour > 23) {
+      hour = hour - 24;
+    }
+    return this.formatNum(hour) + this.formatNum(minute);
+  }
+  
+  timeExtend(weekday, lastTime) {
+    if (weekday[weekday.length - 1) != 0) {
+      
+    
+  
   render() {
+    const timesheetOfEachLine = this.props.timesheetOfEachLine;
+    const timesheetTableDiv = [];
+    const tableHead = <tr><th>方向</th><th>周日-周四</th><th>周五-周六</th></tr>;
+    for (const ele in timesheetOfEachLine) {
+      const direction = ele.description;
+      const firstTime = ele.firstTime;
+      const lastTime = ele.lastTime;
     <div></div>
   }
 }
