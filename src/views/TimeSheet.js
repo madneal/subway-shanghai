4edc09af5ba3ce84426f39ca0c34a7fdf996f3a8
Neal Caffery <neal1991@sina.com>
2018-03-13 15:36:28 +0800

    Update TimeSheet.js

--- src/views/TimeSheet.js
+++ src/views/TimeSheet.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import TimesheetTable from './TimesheetTable'
+import { lineColor } from '../data/Data'
 
 class TimeSheet extends React.Component {
   constructor(props) {
@@ -17,13 +18,30 @@ class TimeSheet extends React.Component {
       currentLine: line
     });
   }
+    
+  getStyle(line) {
+    if (line === this.currentLine) {
+      const style = {
+        background: #eee;
+        color: #777;
+        font-weight: 400
+      };
+    } else {
+      const style = {
+        color: #fff;
+        font-weight: 700;
+        background: lineColor[line]
+      }
+    return style;
+    }
+        
 
   render() {
     const timesheet = this.props.timesheet;
     const timesheetEles = [];
     
     for (const line in timesheet) {
-      const div = <div className="line" key={line}>{line} + '号线'<TimesheetDiv timesheet = {this.state.timesheetOfEachLine} /></div>;
+      const div = <div className="line" key={line} style={this.getStyle(line)}>{line} + '号线'<TimesheetDiv timesheet = {this.state.timesheetOfEachLine} /></div>;
       timesheetEles.push(div);
     }
 
