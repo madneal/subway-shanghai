a9fb0b1ab80af5e6028d11b6af799811050f4880
neal1991 <neal1991@sina.com>
2018-03-13 22:46:07 +0800

    rename

--- src/views/TimesheetTable.js
+++ src/views/TimesheetTable.js
@@ -1,9 +1,6 @@
 import React from 'react'
 
 class TimesheetTable extends React.Component {
-  constructor(props) {
-    super(props)
-  }
   
   formatNum(num) {
     if (num < 10) {
@@ -18,7 +15,7 @@ class TimesheetTable extends React.Component {
     let minute = +timeArr[1];
     minute = minute + minutes;
     if (minute > 60) {
-      hour = hour + parseInt(minute / 60);
+      hour = hour + parseInt((minute / 60), 10);
       minute = minute % 60;
     } 
     if (hour > 23) {
@@ -28,7 +25,7 @@ class TimesheetTable extends React.Component {
   }
   
   timeExtend(weekday, lastTime) {
-    if (weekday[weekday.length - 1] !== 0) {
+    if (weekday && weekday[weekday.length - 1] !== 0) {
       lastTime = this.addTime(lastTime, +weekday[weekday.length -1]);
     }
     return lastTime;
@@ -38,20 +35,23 @@ class TimesheetTable extends React.Component {
   render() {
     const timesheetOfEachLine = this.props.timesheetOfEachLine;
     const timesheetTableDiv = [];
-    const tableHead = <tr><th>方向</th><th>周日-周四</th><th>周五-周六</th></tr>;
-    timesheetTableDiv.push(tableHead);
-    for (const ele in timesheetOfEachLine) {
+    for (const index in timesheetOfEachLine) {
+      const ele = timesheetOfEachLine[index];
       const direction = ele.description;
       const firstTime = ele.firstTime;
       const lastTime = ele.lastTime;
       const lastTimeExtend = this.timeExtend(ele.weekday, lastTime);
-      const style = lastTime === lastTimeExtend ? 'color:red;' : null;
+      const style = lastTime === lastTimeExtend ? {color:'red'} : null;
+      const begin = <span class="begin">始</span>;
       timesheetTableDiv.push(
-        <tr><td>{direction}</td><td>{firstTime} {lastTime}</td><td>{firstTime} <span style={style}>{lastTimeExtend}</span></td></tr>
+        <tr key={direction}><td>{direction}</td><td>{firstTime} {lastTime}</td><td>{firstTime} <span style={style}>{lastTimeExtend}</span></td></tr>
       );
     }
     return (
-      <table>{timesheetTableDiv}</table>
+      <table>
+        <thead><tr><th>方向</th><th>周日-周四</th><th>周五-周六</th></tr></thead>
+        <tbody>{timesheetTableDiv}</tbody>
+      </table>
     )
   }
 }
