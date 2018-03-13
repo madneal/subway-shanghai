684b0665b7e09c444045fca377d029b34590dc40
Neal Caffery <neal1991@sina.com>
2018-03-13 08:55:42 +0800

    Update TimeSheet.js

--- src/views/TimeSheet.js
+++ src/views/TimeSheet.js
@@ -1,16 +1,25 @@
 import React from 'react'
+import TimesheetDiv from './TimesheetDiv'
 
 class TimeSheet extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      timesheetOfEachLine: null
   }
 
   render() {
     const timesheet = this.props.timesheet;
+    const timesheetEles = [];
+    
+    for (const line in timesheet) {
+      const div = <div className="line-sheet">{line}<TimesheetDiv timesheet = {this.state.timesheetOfEachLine} /></div>;
+      timesheetEles.push(div);
+    }
 
     return (
       <div className="timesheet">
-        <div className="line-name"></div>
+      {timesheetEles}
       </div>
     )
   }
