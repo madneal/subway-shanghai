dc00a7275416cc840f95c34e4ba814d65cff8f9e
neal1991 <neal1991@sina.com>
2018-03-15 13:17:08 +0800

    reduce the size of label :zap:

--- src/views/Label.js
+++ src/views/Label.js
@@ -7,7 +7,11 @@ class Label extends React.Component {
     const labelElements = [];
     for (let i = 0; i < labels.length; i++) {
       const label = labels[i];
-      labelElements.push(<text x={label.x} y={label.y} fill={label.fill} key={label.text + i}>{label.text}</text>);
+      if (label.text.lastIndexOf('号线') !== -1) {
+        labelElements.push(<text x={label.x} y={label.y} fill={label.fill} key={label.text + i}>{label.text}</text>);
+      } else {
+        labelElements.push(<text x={label.x} y={label.y} key={label.text+i}>{label.text}</text>);
+      }
     }
     return(
       <g>{labelElements}</g>
