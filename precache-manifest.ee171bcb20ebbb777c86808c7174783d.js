ab58c8dcbabe8ac3a31ff6f13470dc68eb134037
neal1991 <neal1991@sina.com>
2018-10-09 19:51:31 +0800

    Updates

---
+++ precache-manifest.ee171bcb20ebbb777c86808c7174783d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f0878a43a3b5ea7f508",
+    "url": "/subway-shanghai/static/js/runtime~main.3f0878a4.js"
+  },
+  {
+    "revision": "61b1022755db1428735b",
+    "url": "/subway-shanghai/static/js/main.61b10227.chunk.js"
+  },
+  {
+    "revision": "a5ed942880f1105774ca",
+    "url": "/subway-shanghai/static/js/1.a5ed9428.chunk.js"
+  },
+  {
+    "revision": "61b1022755db1428735b",
+    "url": "/subway-shanghai/static/css/main.4dee0569.chunk.css"
+  },
+  {
+    "revision": "961cd133c78138dad62bf51bdf2a43cc",
+    "url": "/subway-shanghai/index.html"
+  }
+];
